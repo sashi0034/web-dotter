e330da4489c7b394deddaf8a37f8f0c71389a21a
sashi0034 <satoshi3104.nishi@gmail.com>
2022-02-23 12:26:04 +0900

    マウスカーソルについていく

--- src/MainCanvas.tsx
+++ src/MainCanvas.tsx
@@ -1,5 +1,7 @@
 
+import { posix } from "node:path/win32";
 import React from "react";
+import { findDOMNode } from "react-dom";
 import "./MainCanvas.css"
 import sadpanda from "./sadpanda_24x24.png"
 
@@ -10,6 +12,7 @@ interface IMainCanvasState
 {
     posX: number,
     posY: number,
+    style: {[name: string]: string}
 }
 
 
@@ -24,14 +27,45 @@ export class MainCanavas extends React.Component<IMainCanvasProps, IMainCanvasSt
         {
             posX: 0,
             posY: 0,
+            style: {left: "0px", top: "0px"}
+        }
+    }
+
+    public override componentDidMount()
+    {
+        let body = document.getElementById("canvas-base");
+        if (body!==null)
+        {
+            const rect = body.getBoundingClientRect();
+
+            this.setState({
+                posX: rect.left + window.pageXOffset,
+                posY: rect.top + window.pageYOffset,
+            });
+
+            document.addEventListener("mousemove", (e)=>{
+                console.log(e.clientX, e.clientY)
+                this.setState({
+                    style: {
+                        left: e.clientX-this.state.posX+"px",
+                        top: e.clientY-this.state.posY+"px"
+                    }
+                });
+            })
         }
     }
 
     public override render(): React.ReactNode 
     {
+
         return (
-            <div className="canvas-name">
-                <img src={sadpanda} alt="サッドパンダの画像" title="sadpanda" className="canvas-content"></img>
+            <div id="canvas-base">
+                <img 
+                    src={sadpanda} 
+                    alt="サッドパンダの画像" 
+                    title="sadpanda" 
+                    id="canvas-body"
+                    style={this.state.style}></img>
             </div>
         ); 
     }
