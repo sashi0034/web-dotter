c1fb72583822f9268845495213ce7ba9a191e5c3
sashi0034 <satoshi3104.nishi@gmail.com>
2022-02-24 09:00:17 +0900

    線を引けるようにした

--- src/MainCanvas.tsx
+++ src/MainCanvas.tsx
@@ -3,7 +3,6 @@ import { posix } from "node:path/win32";
 import React from "react";
 import { findDOMNode } from "react-dom";
 import "./MainCanvas.css"
-import sadpanda from "./sadpanda_24x24.png"
 
 interface IMainCanvasProps
 {
@@ -13,6 +12,9 @@ class MouseState
 {
     public x: number=0;
     public y: number=0;
+    public oldX = 0;
+    public oldY = 0;
+    public isRight: boolean = false;
     public isMiddle: boolean=false;
     public middleFromX: number=0;
     public middleFromY: number=0;
@@ -34,10 +36,15 @@ export class MainCanavas extends React.Component<IMainCanvasProps, IMainCanvasSt
     private oldPosLeft: number=0;
     private oldPosTop: number=0;
     private scale: number = 2;
-    private sizeWidth = 16;
-    private sizeHeight = 16;
+    private sizeWidth = 32;
+    private sizeHeight = 32;
     private stylePos: {[tag: string]: string} = {left: "0px", top: "0px"};
     private styleSize: {[tag: string]: string} = {width: this.sizeWidth*this.scale + "px", height: this.sizeHeight*this.scale+"px"};
+    
+    private canvasElement: HTMLCanvasElement = (null as unknown) as HTMLCanvasElement;
+    private canvasContext: CanvasRenderingContext2D = (null as unknown) as CanvasRenderingContext2D;
+
+    private canLine: boolean = false;
 
     public constructor(props: IMainCanvasProps | Readonly<IMainCanvasProps>)
     {
@@ -54,12 +61,27 @@ export class MainCanavas extends React.Component<IMainCanvasProps, IMainCanvasSt
 
     public override componentDidMount()
     {
-        let body = document.getElementById("canvas-base");
-        if (body!==null)
+        let base = document.getElementById("canvas-base");
+        if (base!==null)
         {
             this.setEvent();
             this.setStateStyle();
+
+            let canvas: HTMLCanvasElement = document.getElementById("canvas-body") as HTMLCanvasElement;
+            if (canvas !== null)
+            {
+                this.canvasElement = canvas;
+                let context = canvas.getContext("2d");
+                if (context!==null)
+                {
+                    context.imageSmoothingEnabled = false;
+                    context.fillStyle="#fff";
+                    context.fillRect(0, 0, this.sizeWidth, this.sizeHeight);
+                    this.canvasContext = context;
+                }
+            }
         }
+
     }
 
     private setEvent()
@@ -75,10 +97,36 @@ export class MainCanavas extends React.Component<IMainCanvasProps, IMainCanvasSt
                     top: this.posTop+"px"
                 }
                 this.setStateStyle();
-                
             }
+            if (this.mouse.isRight===true)
+            {
+                if (this.canLine)
+                {// 線を引く
+                    this.canvasContext.strokeStyle = "#000";
+                    let begin = this.clientToCanvas(this.mouse.oldX, this.mouse.oldY);
+                    let end = this.clientToCanvas(e.clientX, e.clientY);
+                    console.log(begin, end);
+                    this.canvasContext.lineWidth=1;
+                    this.canvasContext.beginPath();
+                    this.canvasContext.moveTo(begin.x, begin.y);
+                    this.canvasContext.lineTo(end.x, end.y);
+                    this.canvasContext.closePath();
+                    this.canvasContext.stroke();
+                }
+                else
+                {
+                    this.canLine = true;
+                }
+            }
+                            
+            this.mouse.oldX = e.clientX;
+            this.mouse.oldY = e.clientY;
         })
         document.addEventListener("mousedown", (e)=>{
+            if (e.button===0)
+            {
+                this.mouse.isRight = true;
+            }
             if (e.button===1) 
             {
                 this.mouse.middleFromX = e.clientX;
@@ -87,6 +135,11 @@ export class MainCanavas extends React.Component<IMainCanvasProps, IMainCanvasSt
             }
         });
         document.addEventListener("mouseup", (e)=>{
+            if (e.button==0)
+            {
+                this.mouse.isRight = false;
+                this.canLine = false;
+            }
             if (e.button===1) 
             {
                 this.oldPosLeft = this.posLeft;
@@ -98,13 +151,29 @@ export class MainCanavas extends React.Component<IMainCanvasProps, IMainCanvasSt
             this.scale += -(this.scale/10) * e.deltaY/100.0;
             this.scale = Math.max(this.scale, 1.0);
             this.styleSize = {width: this.sizeWidth*this.scale + "px", height: this.sizeHeight*this.scale+"px"};
-            console.log(this.styleSize);
+
             this.setStateStyle();
         });
     }
+    private clientToCanvas(clientX: number, clientY: number) 
+    {
+        let rect = this.canvasElement.getBoundingClientRect();
+        let cx = clientX - rect.x + document.body.scrollLeft;
+        let cy = clientY - rect.y + document.body.scrollTop;
+        cx /= this.scale;
+        cy /= this.scale;
+        
+        let ret = 
+        {
+            x: cx|0,
+            y: cy|0
+        };
+        return ret;
+    }
+
     private setStateStyle()
     {
-        this.setState({style: Object.assign({}, this.stylePos, this.styleSize )});
+        this.setState({style: Object.assign({}, this.stylePos, this.styleSize, {border: '2px solid #222'} )});
     }
 
     public override render(): React.ReactNode 
@@ -112,12 +181,12 @@ export class MainCanavas extends React.Component<IMainCanvasProps, IMainCanvasSt
 
         return (
             <div id="canvas-base">
-                <img 
-                    src={sadpanda} 
-                    alt="サッドパンダの画像" 
-                    title="sadpanda" 
+                <canvas 
                     id="canvas-body"
-                    style={this.state.style}></img>
+                    width={this.sizeWidth} 
+                    height={this.sizeHeight}
+                    onContextMenu={() => {return false;}}
+                    style={this.state.style}/>
             </div>
         ); 
     }
